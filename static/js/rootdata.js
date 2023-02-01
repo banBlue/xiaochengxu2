bdea07bd3fd2cec1c76f43924134445754907193
huangmingyi <huangmingyi@smyfinancial.com>
2023-02-01 11:08:07 +0800

    .

---
+++ static/js/rootdata.js
@@ -0,0 +1,32 @@
+[
+  {
+    "chapte": "7",
+    "r": "",
+    "desc": "",
+  },
+  {
+    "chapte": "7",
+    "r": "a",
+    "desc": "去,离开",
+  },
+  {
+    "chapte": "7",
+    "r": "cap",
+    "desc": "头",
+  },
+  {
+    "chapte": "7",
+    "r": "sci",
+    "desc": "知",
+  },
+  {
+    "chapte": "7",
+    "r": "ist",
+    "desc": "人",
+  },
+  {
+    "chapte": "7",
+    "r": "quir=quest",
+    "desc": "寻求",
+  },
+]
